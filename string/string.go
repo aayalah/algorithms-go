088b5a75db3f82a200f606317d6e3234770f8b14
Alejandro Ayala-Hurtado <amayala7@yahoo.com>
2017-11-04 11:54:58 -0700

    fix

--- string/string.go
+++ string/string.go
@@ -27,3 +27,34 @@ func lengthOfLongestSubstring(s string) int {
 	}
 	return length
 }
+
+func longestPalindrome(s string) string {
+	runeArr := []rune(s)
+	table := make([][]bool, len(runeArr))
+
+	for j := 0; j < len(runeArr); j += 1 {
+		table[j] = make([]bool, len(runeArr))
+	}
+
+	ans := ""
+	length := 0
+
+	for palLen := 0; palLen < len(runeArr); palLen += 1 {
+		for i := 0; i <= len(runeArr)-palLen-1; i += 1 {
+			if palLen == 0 {
+				table[i][i+palLen] = true
+			} else if palLen == 1 {
+				table[i][i+palLen] = runeArr[i] == runeArr[i-palLen]
+			} else {
+				table[i][i+palLen] = table[i+1][i+palLen-1] && runeArr[i] == runeArr[i+palLen]
+			}
+
+			if table[i][i+palLen] && (palLen+1) > length {
+				ans = string(runeArr[i : i+palLen+1])
+				length = palLen + 1
+			}
+		}
+	}
+
+	return ans
+}
