0b9b73cb779d3118403f8a5861f1fab5ddb990bc
Alejandro Ayala-Hurtado <amayala7@yahoo.com>
2017-11-04 19:09:21 -0700

    added findDuplicates

--- array/array.go
+++ array/array.go
@@ -24,3 +24,35 @@ func countBattleships(board [][]byte) int {
 	}
 	return numShips
 }
+
+func findOriginalValue(val, convNum, len int) int {
+	for val > len {
+		val -= convNum
+	}
+	return val
+}
+
+func convertNumber(len, num int) int {
+	return len + 1 + num
+}
+
+func findDuplicates(nums []int) []int {
+	for i := 0; i < len(nums); i += 1 {
+
+		val := convertNumber(len(nums), i)
+		index := findOriginalValue(nums[i], val, len(nums))
+		nums[index] += convertNumber(len(nums), index)
+		nums[i] -= index
+	}
+
+	res := make([]int, 0)
+	for j := 0; j < len(nums); j += 1 {
+		val := convertNumber(len(nums), j+1)
+
+		if nums[j]-val > 0 {
+			res = append(res, j+1)
+		}
+	}
+	return res
+
+}
